a3578d7c5a1405e2da85fdcb6361fc274c4f6929
zephyr <songzez@outlook.com>
2026-01-10 22:58:54 +0800

    feat: Quasar config

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,19 @@
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
+import { Quasar } from 'quasar'
 
 import App from './App.vue'
 import router from './router'
 
+// Import icon libraries
+import '@quasar/extras/material-icons/material-icons.css'
+// Import Quasar css
+import 'quasar/src/css/index.sass'
+
 const app = createApp(App)
+
+// UI Framework
+app.use(Quasar, {plugins: {},})
 
 app.use(createPinia())
 app.use(router)
--- vite.config.ts
+++ vite.config.ts
@@ -3,12 +3,20 @@ import { fileURLToPath, URL } from 'node:url'
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueDevTools from 'vite-plugin-vue-devtools'
+import { quasar, transformAssetUrls } from '@quasar/vite-plugin'
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [
-    vue(),
+    vue({
+      template: { transformAssetUrls }
+    }),
     vueDevTools(),
+    quasar({
+      sassVariables: fileURLToPath(
+        new URL('./src/quasar-variables.sass', import.meta.url)
+      )
+    })
   ],
   resolve: {
     alias: {
